294eb559c109a031955c9c46dbac347ffdfeb4dd
Arthur Chen <28554289+aulchen@users.noreply.github.com>
2021-11-02 00:29:25 -0700

    Introduced scaling on u such that u(1) = 1

--- qrHouseholderNaive/reflector.m
+++ qrHouseholderNaive/reflector.m
@@ -2,11 +2,15 @@
 %all entries except the first 0.
 
 function [tau, gamma, u] = reflector(x)
+  n = length(x);
   tau = sqrt(sum(x.^2));
   if x(1) < 0
     tau = -tau;
   endif
   u = x;
   u(1) = x(1) + tau;
-  gamma = 2 / sum(u.^2);
+  %Rescale u
+  u(2:n) = u(2:n) / u(1);
+  gamma = u(1)/tau;
+  u(1) = 1;
 end
