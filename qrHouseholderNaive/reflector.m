87d59943ec737e4a1a7ba92e1c067926dbd7acb8
Arthur Chen <28554289+aulchen@users.noreply.github.com>
2021-11-02 00:31:44 -0700

    Added rescaling on x such that infinity norm(x) = 1

--- qrHouseholderNaive/reflector.m
+++ qrHouseholderNaive/reflector.m
@@ -3,6 +3,14 @@
 
 function [tau, gamma, u] = reflector(x)
   n = length(x);
+  beta = max(abs(x));
+  if beta == 0
+    tau = 0;
+    gamma = 0;
+    u = zeros(n, 1);
+    return
+  endif
+  x = x / beta;
   tau = sqrt(sum(x.^2));
   if x(1) < 0
     tau = -tau;
@@ -13,4 +21,5 @@ function [tau, gamma, u] = reflector(x)
   u(2:n) = u(2:n) / u(1);
   gamma = u(1)/tau;
   u(1) = 1;
+  tau = tau * beta;
 end
