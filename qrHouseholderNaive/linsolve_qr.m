0db6000d403178ae7e4e89bb121638433d6ea3b3
Arthur Chen <28554289+aulchen@users.noreply.github.com>
2021-11-02 01:38:37 -0700

    Implemented QR and linear solving using reflectors

--- qrHouseholderNaive/linsolve_qr.m
+++ qrHouseholderNaive/linsolve_qr.m
@@ -1,7 +1,22 @@
-%Solve a linear system Ax = b using the QR decomposition
+%Solve a linear system Ax = b using QR decomposition
+%b is assumed to be a column vector
 
 function x = linsolve_qr(A, b)
-  [Q, R] = qr_decomposition_naive(A);
-  c = Q'*b;
-  x = backsubs(R, c);
+  n = length(b);
+  %Make b a column vector
+  [A, gamma] = qr_decomposition(A);
+  R = triu(A);
+  %Repeatedly apply reflectors Q1, Q2... Qn-1 to b
+  %Since QTb = Qn-1...Q1b = c
+  for k = 1:n-1
+    %Extract u from A
+    u = ones(n-k+1, 1);
+    u(2:end, 1) = A(k+1:n, k);
+    %Compute Qkb using reflectors
+    vT = gamma(k)*u';
+    vT = vT*b(k:end);
+    b(k:end) = b(k:end) - u*vT;
+  endfor
+  %Backsolve Rx = Qb
+  x = backsubs(A, b);
 end
